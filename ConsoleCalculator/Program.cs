973ca82198748248dcae40a834df8d32dcc4f087
abdullahalhamidi <abdullahalhamidi@outlook.com>
2022-08-10 00:07:02 -0500

    Add project files.

---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//Making a Calculator
+class Calculator
+{
+    double numberOne;
+    double numberTwo;
+
+    public Calculator()
+    {
+        // Get user values that they want to perform an operation on
+        Console.WriteLine("Please Enter a number");
+        string numberOneValue = Console.ReadLine();
+
+        Console.WriteLine("Please enter an operation like - + * / or %");
+        string operationsSymbol = Console.ReadLine();
+
+        Console.WriteLine("Please Enter another number");
+        string numberTwoValue = Console.ReadLine();
+
+        //Converting our values to double since it's a string
+        double numberOneValueConverted = Convert.ToDouble(numberOneValue);
+        double numberTwoValueConverted = Convert.ToDouble(numberTwoValue);
+
+
+        //Performing the operation on the values
+        if (operationsSymbol == "-")
+        {
+            Console.WriteLine(numberOneValueConverted - numberTwoValueConverted);
+        }
+        else if (operationsSymbol == "+")
+        {
+            Console.WriteLine(numberOneValueConverted + numberTwoValueConverted);
+        }
+        else if (operationsSymbol == "*")
+        {
+            Console.WriteLine(numberOneValueConverted * numberTwoValueConverted);
+        }
+        else if (operationsSymbol == "/")
+        {
+            Console.WriteLine(numberOneValueConverted / numberTwoValueConverted);
+        }
+        else if (operationsSymbol == "%")
+        {
+            Console.WriteLine(numberOneValueConverted % numberTwoValueConverted);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calOne = new Calculator();
+    }
+}
